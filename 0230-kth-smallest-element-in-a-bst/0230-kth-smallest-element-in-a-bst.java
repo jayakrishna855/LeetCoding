148ee64ad0e9c86d8bca5ffbed1356fdcf4b631c
Jaya Krishna Pasham <69287549+jayakrishna855@users.noreply.github.com>
2026-05-13 09:06:09 -0400

    Time: 0 ms (100%), Space: 46.9 MB (32.6%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -15,15 +15,25 @@
  */
 class Solution {
     int ans, currK;
-    public void findkthSmallest(TreeNode root, int k){
-        if(root == null) return;
-        findkthSmallest(root.left, k);
-        this.currK+=1;
-        if(currK == k) this.ans = root.val;
-        findkthSmallest(root.right, k);
+    // public void findkthSmallest(TreeNode root, int k){
+    //     if(root == null) return;
+    //     findkthSmallest(root.left, k);
+    //     this.currK+=1;
+    //     if(currK == k) this.ans = root.val;
+    //     findkthSmallest(root.right, k);
+    // }
+    public int inorder(TreeNode root, int k){
+        if(root == null) return 0;
+        int left = inorder(root.left, k);
+        if(left!=0) return left;
+        currK++;
+        if(currK == k) return root.val;
+        int right = inorder(root.right, k);
+        return right;
     }
     public int kthSmallest(TreeNode root, int k) {
-        findkthSmallest(root, k);
-        return this.ans;
+        // findkthSmallest(root, k);
+        return inorder(root, k);
+        //return this.ans;
     }
 }
