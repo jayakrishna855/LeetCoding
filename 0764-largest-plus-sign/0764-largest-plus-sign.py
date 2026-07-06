e271cf103099395c18b608d0a3852254dabb23c7
Jaya Krishna Pasham <69287549+jayakrishna855@users.noreply.github.com>
2026-07-06 19:42:36 -0400

    Time: 2473 ms (9.14%), Space: 22.5 MB (78.49%) - LeetHub

--- 0764-largest-plus-sign/0764-largest-plus-sign.py
+++ 0764-largest-plus-sign/0764-largest-plus-sign.py
@@ -1,22 +1,8 @@
 class Solution:
-    def orderOfLargestPlusSign(self, N: int, mines: List[List[int]]) -> int:
-        rows = [[-1, N] for _ in range(N)]
-        cols = [[-1, N] for _ in range(N)]
-        for r, c in mines:
-            rows[r].append(c)
-            cols[c].append(r)
-        for i in range(N):
-            rows[i].sort()
-            cols[i].sort()
-        mxp = 0
-        for r in range(N):
-            for i in range(len(rows[r]) - 1):
-                left_b = rows[r][i]
-                right_b = rows[r][i + 1]
-                for c in range(left_b + mxp + 1, right_b - mxp):
-                    idx = bisect_right(cols[c], r) - 1
-                    up_b = cols[c][idx]
-                    down_b = cols[c][idx + 1]
-                    mxp = max(mxp, min(c - left_b, right_b - c, r - up_b, down_b - r))
-        return mxp
-        
+    def orderOfLargestPlusSign(self, n: int, mines: List[List[int]]) -> int:
+        g = [[min(i, n - 1 - i, j, n - 1 - j) + 1 for j in range(n)] for i in range(n)]
+        for x, y in mines:
+            for i in range(n):
+                g[i][y] = min(g[i][y], abs(i - x))
+                g[x][i] = min(g[x][i], abs(i - y))
+        return max([max(row) for row in g])
